b0ccf1c06d99beec14486212dd329badb48cd381
Anna Flávia <anna.castro@truckpad.com.br>
2020-05-24 17:39:03 -0300

    Add tests integration

---
+++ backend/tests/integration/ong.spec.js
@@ -0,0 +1,31 @@
+const request = require('supertest');
+
+const app = require('../../src/app');
+
+const connection = require('../../src/database/connection');
+
+describe('ONG', () => {
+  beforeEach(async () => {
+    await connection.migrate.rollback();
+    await connection.migrate.latest();
+  });
+
+  afterAll(async () => {
+    await connection.destroy();
+  });
+
+  it('should be able to create a new ONG', async () => {
+    const response = await request(app)
+      .post('/ongs')
+      .send({
+        name: "ONG Show",
+        email: "annaflavia.castros@hotmail.com.br",
+        whatsapp: "11949658506",
+        city: "São Bernardo do Campo",
+        uf: "SP"
+      });
+
+    expect(response.body).toHaveProperty('id');
+    expect(response.body.id).toHaveLength(8);
+  });
+});
